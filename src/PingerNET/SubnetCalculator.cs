56554cd93749a20a537ac5e4e81ff87c8076936f
Luca Mauri <lucamauricom@hotmail.com>
2023-01-23 23:13:28 +0100

    New structure and first calculator class

---
+++ src/PingerNET/SubnetCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace SynthesisCode.PingerNET;
+
+public class SubnetCalculator
+{
+    public static IPAddress GetNetworkAddress(IPAddress address, IPAddress subnetMask)
+    {
+        byte[] ipAdressBytes = address.GetAddressBytes();
+        byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+        if (ipAdressBytes.Length != subnetMaskBytes.Length)
+        {
+            throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+        }
+
+        byte[] networkAddressBytes = new byte[ipAdressBytes.Length];
+        for (int i = 0; i < ipAdressBytes.Length; i++)
+        {
+            networkAddressBytes[i] = (byte)(ipAdressBytes[i] & subnetMaskBytes[i]);
+        }
+
+        return new IPAddress(networkAddressBytes);
+    }
+}
