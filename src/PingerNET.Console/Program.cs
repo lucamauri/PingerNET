1bf77ea0d0165373f3636927981d688a0455ff7b
Luca Mauri <lucamauricom@hotmail.com>
2023-01-24 23:21:17 +0100

    First pinger test

--- src/PingerNET.Console/Program.cs
+++ src/PingerNET.Console/Program.cs
@@ -4,7 +4,17 @@
 using SynthesisCode.PingerNET;
 using System.Net;
 
+//TEST for calculator
 IPCalculator IPCalc = new IPCalculator();
-
-
 Console.WriteLine(IPCalc.GetNetworkAddress(IPAddress.Parse("192.168.1.15"), IPAddress.Parse("255.255.255.0")));
+
+//TEST for pinger
+Pinger networkHelper = new Pinger();
+string[] ips = { "192.168.1.1", "192.168.1.2", "192.168.1.3" };
+var replies = await networkHelper.PingIpsAsync(ips);
+Console.WriteLine(String.Format("|{0,40}|{1,15}|{2,30}|{3,17}|", "DNS", "Address", "Status", "Round trip time"));
+Console.WriteLine(new String('=', 107));
+foreach (var reply in replies)
+{
+    Console.WriteLine(String.Format("|{0,40}|{1,15}|{2,30}|{3,17}|", Dns.GetHostEntry(reply.Address).HostName, reply.Address, reply.Status, reply.RoundtripTime));
+}
---
+++ src/PingerNET/Pinger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthesisCode.PingerNET;
+
+public class Pinger
+{
+    public async Task<PingReply[]> PingIpsAsync(string[] ips)
+    {
+        var pingTasks = ips.Select(ip => new Ping().SendPingAsync(ip)).ToArray();
+        return await Task.WhenAll(pingTasks);
+    }
+}
